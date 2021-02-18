c8a6e56caf64a40608d04cb3ff9cb81d5c94cde3
KriszProg <krisz.codecool@gmail.com>
2021-02-18 11:12:30 +0100

    Initial commit

---
+++ src/main/java/hu/kriszprog/cvcreatorbackend/CvCreatorBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.kriszprog.cvcreatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvCreatorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvCreatorBackendApplication.class, args);
+    }
+
+}
