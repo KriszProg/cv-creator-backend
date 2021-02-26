4500f28d9e7b05fbaca90db7f1aea3fa0e3836b6
KriszProg <krisz.codecool@gmail.com>
2021-02-26 18:14:10 +0100

    add new features which update the CV (bind the incoming title and 'Candidate' object to it)

--- src/main/java/hu/kriszprog/cvcreatorbackend/repository/CandidateRepository.java
+++ src/main/java/hu/kriszprog/cvcreatorbackend/repository/CandidateRepository.java
@@ -2,7 +2,12 @@ package hu.kriszprog.cvcreatorbackend.repository;
 
 import hu.kriszprog.cvcreatorbackend.entity.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
     Candidate getCandidateById(Long id);
+
+    @Query("SELECT c FROM Candidate c WHERE c.name = :name AND c.role = :role")
+    Candidate getCandidateIfExist(@Param("name") String name, @Param("role") String role);
 }
